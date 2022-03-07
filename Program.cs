76e5d51bac2b69d4a55b30787d1c7a17a0658607
gudivadasusmitha <Gudivada.Susmitha@ibm.com>
2022-03-07 12:29:43 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace access_modifiers
+{
+    /*
+     ////**********public specifier*****************
+    class specifier
+    {
+        public string type = "public";
+    }
+    class access
+    {
+        static void Main(string[] args)
+        {
+            specifier myObj = new specifier();
+            Console.WriteLine(myObj.type);
+            Console.ReadLine();
+        }
+    }
+    */
+    /*
+    
+    ////*****private specifier*****
+    class specifier1
+    {
+        private String name = "susmi";
+        public static void Main()
+        {
+            specifier1 myobj = new specifier1();
+            Console.WriteLine(myobj.name);
+            Console.ReadLine();
+        }
+    }
+    */
+    
+    /*
+    // **********protected*************
+    class X
+    {
+        protected int x;
+        public X()
+        {
+            x = 10;
+        }
+    }
+    class y:X
+    {
+        public int getX()
+        {
+            return x;
+        }
+    }
+    class Protect
+    {
+        static void Main (string[] args)
+        {
+            X obj1 = new X();
+            y obj2 = new y();
+            Console.WriteLine("value of x is:{0}", obj2.getX());
+            Console.ReadLine();
+        }
+    }
+    */
+    //
+
+
+}
